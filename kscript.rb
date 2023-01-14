33dc12a46e0670fa8300773cb8a8d0f2f93bbf8f
aartiPl <aartiPl@users.noreply.github.com>
2023-01-14 09:33:22 +0000

    v4.2.0 release

--- kscript.rb
+++ kscript.rb
@@ -1,8 +1,8 @@
 class Kscript < Formula
   desc "kscript"
   homepage "https://github.com/kscripting/kscript"
-  url "https://github.com/kscripting/kscript/releases/download/v4.2.0-RC.3/kscript-4.2.0-RC.3-bin.zip"
-  sha256 "f34f95d82190ae95585b2c22c6feb4c7313b25ca53a31a8b52161fb9060ec2c0"
+  url "https://github.com/kscripting/kscript/releases/download/v4.2.0/kscript-4.2.0-bin.zip"
+  sha256 "35b47b51b3724e4f69c357f443bb4e9d13208d435d5d8478a2c4b48dfa18c395"
   
   depends_on "kotlin"
   
